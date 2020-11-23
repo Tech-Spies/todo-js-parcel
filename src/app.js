7b5fa894bcb3328e57977d648f91d712e0244098
Sayeed Afridi <as.afridi563@gmail.com>
2020-11-23 21:21:19 +0600

    wrapper card in js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
-import Title from './components/Title'
-import Input from './components/Input'
+import Input from "./components/Input";
+import Title from "./components/Title";
 
 const App = () => {
 	const template = `
-		<div class='wrapper'>
-			${Title('My TODO App')}
-			${Input()}
+	<div class="card">
+		${Title('Todo List')}
+		${Input({placeholder: 'Add a todo', icon: 'far fa-calendar-plus'})}
+		<div class="items">
+			<h3>Lorem ipsum dolor sit amet.</h3>
 		</div>
+	</div>
 	`
 	return template
 };
